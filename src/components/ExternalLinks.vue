5dc271d278e29da7feb67d323000d33367cf29fb
The Kicker <cav.lemasters@geneva.edu>
2020-05-29 10:55:48 -0400

    :art:

---
+++ src/components/ExternalLinks.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="Links">
+    <div>
+      <div class="text-center text-dark">
+        <h2>External Links</h2>
+        <hr />
+      </div>
+
+      <div class="card-group m-4 width-limit mx-auto">
+        <div class="card m-2">
+          <img src="../assets/nasaVAB.jpg" class="card-img-top marketingImage" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title text-dark">NASA</h5>
+            <hr />
+            <p
+              class="card-text"
+            >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text text-center">
+              <a
+                href="https://www.nasa.gov"
+                class="btn btn-dark"
+                target="_blank"
+                rel="noopener"
+              >NASA.gov</a>
+            </p>
+          </div>
+        </div>
+
+        <div class="card m-2">
+          <img src="../assets/spacex.jpg" class="card-img-top marketingImage" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">SpaceX</h5>
+            <hr />
+            <p
+              class="card-text"
+            >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text text-center">
+              <a
+                href="https://www.spacex.com/"
+                class="btn btn-dark"
+                target="_blank"
+                rel="noopener"
+              >SpaceX.com</a>
+            </p>
+          </div>
+        </div>
+
+        <div class="card m-2">
+          <img src="../assets/rocketLab.jpg" class="card-img-top marketingImage" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Rocket Labs</h5>
+            <hr />
+            <p
+              class="card-text"
+            >This card has supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text text-center">
+              <a
+                href="https://www.rocketlabusa.com/"
+                class="btn btn-dark"
+                target="_blank"
+                rel="noopener"
+              >RocketLabUSA.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card-group m-4 width-limit mx-auto">
+        <div class="card m-2">
+          <img src="../assets/kerbal.jpg" class="card-img-top marketingImage" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title text-dark">Kerbal Space Program</h5>
+            <hr />
+            <p
+              class="card-text"
+            >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text text-center">
+              <a
+                href="https://www.kerbalspaceprogram.com/"
+                class="btn btn-dark"
+                target="_blank"
+                rel="noopener"
+              >KerbalSpaceProgram.com</a>
+            </p>
+          </div>
+        </div>
+
+        <div class="card m-2">
+          <img src="../assets/reddit.jpeg" class="card-img-top marketingImage" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Reddit/r/spaceporn</h5>
+            <hr />
+            <p
+              class="card-text"
+            >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text text-center">
+              <a
+                href="https://www.reddit.com/r/spaceporn/"
+                class="btn btn-dark"
+                target="_blank"
+                rel="noopener"
+              >Reddit</a>
+            </p>
+          </div>
+        </div>
+
+        <div class="card m-2">
+          <img src="../assets/spacecom.png" class="card-img-top marketingImage" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Space.com</h5>
+            <hr />
+            <p
+              class="card-text"
+            >This card has supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text text-center">
+              <a
+                href="https://www.space.com/"
+                class="btn btn-dark"
+                target="_blank"
+                rel="noopener"
+              >Space.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ExternalLinks",
+  data() {
+    return {
+      Hello: "Hello World!"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.marketingImage {
+  height: 15rem;
+}
+.viewport100h {
+  height: 100vh;
+  width: 100%;
+}
+.viewportText {
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  vertical-align: middle;
+}
+.width-limit {
+  max-width: 1632px;
+  margin: auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Homepage />
+    <ExternalLinks />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Homepage from "@/components/Homepage.vue";
+import ExternalLinks from "@/components/ExternalLinks.vue";
 
 export default {
   name: "Home",
   components: {
-    Homepage
+    Homepage,
+    ExternalLinks
   }
 };
 </script>
