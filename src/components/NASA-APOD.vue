609fe9221c98ddd9e6f5b8aced5a182fc32648f6
The Kicker <cav.lemasters@geneva.edu>
2020-06-01 14:08:08 -0400

    NASA APOD creation :sparkles:

---
+++ src/components/NASA-APOD.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="APOD my-4 mx-4">
+    <div class="card container">
+      <div class="card-body p-3 p-md-6 m-md-3">
+        <h3 class="text-left text-secondary">NASA Astronomy Picture of the Day:</h3>
+        <hr />
+        <h1 class="text-center my-4">{{results.title}}</h1>
+        <div class="row">
+          <div class="mx-auto">
+            <img :src="results.url" class="NASA-apod" />
+          </div>
+        </div>
+        <div class="row container mx-auto">
+          <div class="my-2">
+            <p class="my-2">{{results.explanation}}</p>
+          </div>
+        </div>
+        <hr />
+        <p class="text-center">
+          <i>Last Updated at {{(new Date(results.date).toLocaleString())}}, Check back tomorrow for a new picture of the day!</i>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+let url =
+  "https://api.nasa.gov/planetary/apod?api_key=PAdDVGUIS5Kjq97mf6JzKNKT7TohmSuSaLHAVImo";
+window.axios = require("axios");
+
+export default {
+  name: "NASAapod",
+  data() {
+    return {
+      results: []
+    };
+  },
+  mounted() {
+    window.axios.get(url).then(response => {
+      this.results = response.data;
+    });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <Homepage />
+    <NASAapod />
     <ExternalLinks />
   </div>
 </template>
@@ -8,12 +9,14 @@
 <script>
 // @ is an alias to /src
 import Homepage from "@/components/Homepage.vue";
+import NASAapod from "@/components/NASA-APOD.vue";
 import ExternalLinks from "@/components/ExternalLinks.vue";
 
 export default {
   name: "Home",
   components: {
     Homepage,
+    NASAapod,
     ExternalLinks
   }
 };
