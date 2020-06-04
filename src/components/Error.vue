c56923c813290d012ff8ec16c667650dfc8af9b4
The Kicker <cav.lemasters@geneva.edu>
2020-06-04 16:59:33 -0400

    SEO Update, Sitemap & Robots files added :art:

---
+++ src/components/Error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="container my-4">
+      <h2>{{code}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error404",
+  data() {
+    return {
+      code: "Error 404"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Error404 />
   </div>
 </template>
+
+
+<script>
+// @ is an alias to /src
+import Error404 from "@/components/Error.vue";
+
+export default {
+  name: "About",
+  components: {
+    Error404
+  }
+};
+</script>
