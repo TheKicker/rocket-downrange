7e10cb318a07c8e2a47f7c1aed946b1f6f917b9c
The Kicker <cav.lemasters@geneva.edu>
2020-07-09 18:00:23 -0400

    Minor package updates

---
+++ src/components/Test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>{{results}}</h2>
+  </div>
+</template>
+
+<script>
+let url =
+  "https://friendly-sammet-c73f41.netlify.app/.netlify/functions/products.json";
+window.axios = require("axios");
+
+export default {
+  name: "Test",
+  data() {
+    return {
+      results: []
+    };
+  },
+  mounted() {
+    window.axios.get(url).then(response => {
+      this.results = response.data;
+    });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
