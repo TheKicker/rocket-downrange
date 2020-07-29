c95eb2a9a579af7cd28da66869b7be1e9557040f
The Kicker <cav.lemasters@geneva.edu>
2020-07-29 09:49:11 -0400

    Contact Form fix #5

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,3 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-
 module.exports = {
-  configureWebpack: () => {
-    if (process.env.NODE_ENV !== 'production') return;
-    return {
-      plugins: [
-        new PrerenderSPAPlugin(
-          // Absolute path to compiled SPA
-          path.resolve(__dirname, 'dist'),
-          // List of routes to prerender
-          ['/', '/spacex', '/rocket-lab'],
-          {
-            // options
-          }
-        ),
-      ]
-    }
-  }
+  lintOnSave: false
 }
