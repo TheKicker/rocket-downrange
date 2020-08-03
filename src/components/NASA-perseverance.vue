78446c8d4c040c8a4d127b76b924d55208dfe21e
The Kicker <cav.lemasters@geneva.edu>
2020-08-03 09:43:12 -0400

    Minor ui changes :art:

--- src/components/NASA-perseverance.vue
+++ src/components/NASA-perseverance.vue
@@ -4,7 +4,7 @@
       <div class="card-body p-3 p-md-6 m-md-3">
         <h5 class="text-left text-secondary">NASA Special Event:</h5>
         <hr />
-        <h3 class="text-center my-4">Perseverance : Countdown to Mars</h3>
+        <h3 class="text-center my-4">Perseverance : In Transit to Mars</h3>
         <div class="row">
           <div class="mx-auto">
             <img
@@ -14,23 +14,42 @@
             />
           </div>
         </div>
-        <div class="row container mx-auto">
-          <div class="my-4 row">
-            <div class="p-4 row mx-auto">
-              <a href="https://www.youtube.com/watch?v=JIB3JbIIbPU" target="_blank">
-                <button class="btn btn-block btn-outline-primary italic">Watch Now</button>
-              </a>
-            </div>
-            <div class="p-4 row mx-auto">
-              <a
-                href="https://www.nasa.gov/sites/default/files/atoms/files/mars_2020_launch_press_kit.pdf"
-                target="_blank"
-              >
-                <button class="btn btn-block btn-outline-primary italic">Press Kit</button>
-              </a>
-            </div>
+        <div class="container">
+          <div class="row my-2">
+            <p>
+              NASA’s next mission to Mars — the Mars 2020 Perseverance rover mission — was launched from Cape
+              Canaveral Air Force Station on July 30, 2020. Currently in transit, it will land in Jezero Crater on the Red Planet on Feb.
+              18, 2021. Perseverance is the most sophisticated rover NASA has ever sent to Mars, with a name that embodies
+              NASA’s passion for taking on and overcoming challenges. It will search for signs of ancient microbial life,
+              characterize the planet’s geology and climate, collect carefully selected and documented rock and sediment
+              samples for possible return to Earth, and pave the way for human exploration beyond the Moon.
+            </p>
+          </div>
+          <div class="row my-2 text-dark">
+            <p class="col text-center">
+              <strong>Launch Date (Earth): July 30, 2020</strong>
+            </p>
+            <p class="col text-center">
+              <strong>Landing Date (Mars): February 18, 2021 (estimate)</strong>
+            </p>
+          </div>
+          <div class="my-4 row justify-content-center">
+            <a href="https://www.youtube.com/watch?v=JIB3JbIIbPU" target="_blank" class="py-4 p-3">
+              <button class="btn btn-block btn-outline-primary italic">NASA Launch Webcast</button>
+            </a>
+
+            <a
+              href="https://www.nasa.gov/sites/default/files/atoms/files/mars_2020_launch_press_kit.pdf"
+              target="_blank"
+              class="py-4 p-3"
+            >
+              <button class="btn btn-block btn-outline-primary italic">NASA Press Kit</button>
+            </a>
           </div>
           <hr />
+          <p class="text-center">
+            <i>Last Update: August 3, 2020</i>
+          </p>
         </div>
       </div>
     </div>
--- src/components/SpaceX-Special.vue
+++ src/components/SpaceX-Special.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="Perseverance my-4 mx-4">
+  <div class="SpaceX-Special my-4 mx-4">
     <div class="card container">
       <div class="card-body p-3 p-md-6 m-md-3">
         <h5 class="text-left text-secondary">SpaceX Special Event:</h5>
         <hr />
-        <h3 class="text-center my-4">Dragon's Return : Hurley & Behnken return home</h3>
+        <h3 class="text-center my-4">NASA & SpaceX - Crew Dragon Demo-2 Mission</h3>
         <div class="row">
           <div class="mx-auto">
             <img
@@ -14,23 +14,22 @@
             />
           </div>
         </div>
-        <div class="row container mx-auto">
-          <div class="my-4 row">
-            <div class="p-4 row mx-auto">
-              <a href="https://www.youtube.com/watch?v=sl2jo1bSxl8" target="_blank">
-                <button class="btn btn-block btn-outline-primary italic">Watch Now</button>
-              </a>
-            </div>
-            <!-- <div class="p-4 row mx-auto">
-              <a
-                href="https://www.nasa.gov/sites/default/files/atoms/files/mars_2020_launch_press_kit.pdf"
-                target="_blank"
-              >
-                <button class="btn btn-block btn-outline-primary italic">Press Kit</button>
-              </a>
-            </div>-->
+        <div class="container">
+          <div class="row my-2">
+            <p>A mission that was a longtime in the making, Crew Demo-2 was a manned test flight of the Crew Dragon spacecraft developed by SpaceX. The launch occured on May 30th of 2020, the astronauts spent 2 months in low earth orbit at the International Space Station and then returned to Earth on August 2nd of 2020. This event marks the first time since the final Space Shuttle mission (STS-135) in 2011 that American astronauts were launched on American rockets from American soil. The mission was manned by Commander Douglas Hurley and Joint-Operations Commander Robert Behnken and lasted 63 days - 9 hours and 8 minutes.</p>
+          </div>
+          <div class="my-4 row justify-content-center">
+            <a href="https://www.youtube.com/watch?v=xY96v0OIcK4" target="_blank" class="py-4 p-3">
+              <button class="btn btn-block btn-outline-primary italic">Part 1 - Launch to ISS</button>
+            </a>
+            <a href="https://www.youtube.com/watch?v=sl2jo1bSxl8" target="_blank" class="py-4 p-3">
+              <button class="btn btn-block btn-outline-primary italic">Part 2 - Returning Home</button>
+            </a>
           </div>
           <hr />
+          <p class="text-center">
+            <i>Last Update: August 3, 2020</i>
+          </p>
         </div>
       </div>
     </div>
