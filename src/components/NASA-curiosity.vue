20a54ec15339067708eaeb6902d3f44047245bee
The Kicker <cav.lemasters@geneva.edu>
2020-08-04 11:40:20 -0400

    Curiousity Bug :bug:

--- src/components/NASA-curiosity.vue
+++ src/components/NASA-curiosity.vue
@@ -4,10 +4,10 @@
       <div class="card-body p-3 p-md-6 m-md-3">
         <h5 class="text-left text-secondary">Mars Curiosity Rover Picture of the Day:</h5>
         <hr />
-        <h3 class="text-center my-4">Photo ID#{{this.results.photos[0].id}}</h3>
+        <h3 class="text-center my-4">Photo ID#{{Object.values(results.photos)[0].id}}</h3>
         <div class="row">
           <div class="mx-auto">
-            <a :href="this.results.photos[0].img_src" target="_blank" rel="noopener">
+            <a :href="Object.values(results.photos)[0].img_src" target="_blank" rel="noopener">
               <img
                 :src="secureURL"
                 class="img-fluid"
@@ -22,7 +22,7 @@
             <div class="mx-auto my-2">
               <strong>
                 <a
-                  :href="this.results.photos[0].img_src"
+                  :href="Object.values(results.photos)[0].img_src"
                   target="_blank"
                   rel="noopener"
                   class="btn btn-block btn-outline-primary text-secondary italic"
@@ -31,7 +31,7 @@
             </div>
             <p
               class="my-2 text-primary"
-            >This picture was taken on Earth date: {{new Date(this.results.photos[0].earth_date).toDateString()}}, or Sol {{this.results.photos[0].sol}} on Mars. Captured with the {{this.results.photos[0].camera.full_name}} ({{this.results.photos[0].camera.name}}) aboard the {{this.results.photos[0].rover.name}} rover. {{this.results.photos[0].rover.name}} was launched from Earth on {{new Date(this.results.photos[0].rover.launch_date).toDateString()}} and has been on Mars since {{new Date(this.results.photos[0].rover.landing_date).toDateString()}} and remains {{this.results.photos[0].rover.status}} to this day. All images and mission data compliments of NASA.</p>
+            >This picture was taken on Earth date: {{new Date(Object.values(results.photos)[0].earth_date).toDateString()}}, or Sol {{Object.values(results.photos)[0].sol}} on Mars. Captured with the {{Object.values(results.photos)[0].camera.full_name}} ({{Object.values(results.photos)[0].camera.name}}) aboard the {{Object.values(results.photos)[0].rover.name}} rover. {{Object.values(results.photos)[0].rover.name}} was launched from Earth on {{new Date(Object.values(results.photos)[0].rover.launch_date).toDateString()}} and has been on Mars since {{new Date(Object.values(results.photos)[0].rover.landing_date).toDateString()}} and remains {{Object.values(results.photos)[0].rover.status}} to this day. All images and mission data compliments of NASA.</p>
           </div>
         </div>
         <hr />
@@ -42,10 +42,12 @@
 
 
 <script>
-const day = new Date().getDate() - 6;
-const month = new Date().getMonth() + 1;
-const year = new Date().getFullYear();
-const fullDate = `${year}-${month}${day}`;
+var oneWeekAgo = new Date();
+oneWeekAgo.setDate(oneWeekAgo.getDate() - 9);
+const lastYear = oneWeekAgo.getFullYear();
+const lastMonth = oneWeekAgo.getMonth();
+const lastDay = oneWeekAgo.getDate();
+
 const secure = "";
 const api_key = "PAdDVGUIS5Kjq97mf6JzKNKT7TohmSuSaLHAVImo";
 window.axios = require("axios");
@@ -58,11 +60,7 @@ export default {
     };
   },
   mounted() {
-    var url =
-      "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" +
-      fullDate +
-      "&api_key=" +
-      api_key;
+    var url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${lastYear}-${lastMonth}-${lastDay}&api_key=${api_key}`;
     console.log("NASA Curiosity - " + url);
     window.axios
       .get(url)
@@ -76,7 +74,6 @@ export default {
       var baseURL = this.results.photos[0].img_src;
       if (baseURL.slice(0, 5) != "https") {
         var secure_url = baseURL.slice(0, 4) + "s" + baseURL.slice(4);
-        // console.log("Curiousity Secure Pic - ", secure_url);
         return secure_url;
       } else {
         return baseURL;
--- src/components/NASA-opportunity.vue
+++ src/components/NASA-opportunity.vue
@@ -4,10 +4,10 @@
       <div class="card-body p-3 p-md-6 m-md-3">
         <h5 class="text-left text-secondary">Mars Opportunity Rover Picture of the Day:</h5>
         <hr />
-        <h3 class="text-center my-4">Photo ID#{{this.results.photos[0].id}}</h3>
+        <h3 class="text-center my-4">Photo ID#{{Object.values(results.photos)[0].id}}</h3>
         <div class="row">
           <div class="mx-auto">
-            <a :href="this.results.photos[0].img_src" target="_blank" rel="noopener">
+            <a :href="Object.values(results.photos)[0].img_src" target="_blank" rel="noopener">
               <img
                 :src="secureURL"
                 class="img-fluid"
@@ -22,7 +22,7 @@
             <div class="mx-auto my-2">
               <strong>
                 <a
-                  :href="this.results.photos[0].img_src"
+                  :href="Object.values(results.photos)[0].img_src"
                   target="_blank"
                   rel="noopener"
                   class="btn btn-block btn-outline-primary text-secondary italic"
@@ -31,7 +31,7 @@
             </div>
             <p
               class="my-2 text-primary"
-            >This picture was taken on Earth date: {{new Date(this.results.photos[0].earth_date).toDateString()}}, or Sol {{this.results.photos[0].sol}} on Mars. Captured with the {{this.results.photos[0].camera.full_name}} ({{this.results.photos[0].camera.name}}) aboard the {{this.results.photos[0].rover.name}} rover. {{this.results.photos[0].rover.name}} was launched from Earth on {{new Date(this.results.photos[0].rover.launch_date).toDateString()}} and has been on Mars since {{new Date(this.results.photos[0].rover.landing_date).toDateString()}}. Sadly, we lost communication with {{this.results.photos[0].rover.name}} in June of 2018 and mission was declared {{this.results.photos[0].rover.status}} on February 13th, 2019. All images and mission data compliments of NASA. RIP Oppy.</p>
+            >This picture was taken on Earth date: {{new Date(Object.values(results.photos)[0].earth_date).toDateString()}}, or Sol {{Object.values(results.photos)[0].sol}} on Mars. Captured with the {{Object.values(results.photos)[0].camera.full_name}} ({{Object.values(results.photos)[0].camera.name}}) aboard the {{Object.values(results.photos)[0].rover.name}} rover. {{Object.values(results.photos)[0].rover.name}} was launched from Earth on {{new Date(Object.values(results.photos)[0].rover.launch_date).toDateString()}} and has been on Mars since {{new Date(Object.values(results.photos)[0].rover.landing_date).toDateString()}}. Sadly, we lost communication with {{Object.values(results.photos)[0].rover.name}} in June of 2018 and mission was declared {{Object.values(results.photos)[0].rover.status}} on February 13th, 2019. All images and mission data compliments of NASA. RIP Oppy.</p>
           </div>
         </div>
         <hr />
