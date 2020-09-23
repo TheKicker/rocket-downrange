d11815319044d2b14a99ef6b42fe1382ce23daf0
The Kicker <cav.lemasters@geneva.edu>
2020-09-23 11:22:10 -0400

    Changed external links text :art:

--- src/components/ExternalLinks.vue
+++ src/components/ExternalLinks.vue
@@ -6,19 +6,27 @@
       <!-- About Section -->
       <div class="my-4 width-limit mx-auto">
         <div>
-          <div class="position-relative overflow-hidden text-center bg-primary text-white">
+          <div
+            class="position-relative overflow-hidden text-center bg-primary text-white"
+          >
             <div class="col-md-6 p-lg-5 mx-auto my-5">
-              <h2 class="font-weight-normal text-white">About Rocket Downrange</h2>
+              <h2 class="font-weight-normal text-white">
+                About Rocket Downrange
+              </h2>
               <hr color="white" />
-              <p
-                class="lead font-weight-normal"
-              >Breaking boundaries, setting standards and transforming humanity for the better. Space has always been the final frontier that keeps on giving. This site serves to be a side project that informs and inspires the next generation of explorers.</p>
+              <p class="lead font-weight-normal">
+                Breaking boundaries, setting standards and transforming humanity
+                for the better. Space has always been the final frontier that
+                keeps on giving. This site serves to be a side project that
+                informs and inspires the next generation of explorers.
+              </p>
               <a
                 class="btn btn-outline-light my-4"
                 href="https://github.com/TheKicker/rocket-downrange"
                 target="_blank"
                 rel="noopener"
-              >{{code}}</a>
+                >{{ code }}</a
+              >
             </div>
           </div>
         </div>
@@ -30,8 +38,16 @@
             image="space-force.jpg"
             link="https://www.spaceforce.mil/"
           />
-          <External site="NASA" image="nasaVAB.jpg" link="https://www.nasa.gov" />
-          <External site="European Space Agency" image="ESA.png" link="https://www.esa.int/" />
+          <External
+            site="NASA"
+            image="nasaVAB.jpg"
+            link="https://www.nasa.gov"
+          />
+          <External
+            site="European Space Agency"
+            image="ESA.png"
+            link="https://www.esa.int/"
+          />
           <External
             site="Russian Space Agency"
             image="roscosmos.jpg"
@@ -40,9 +56,21 @@
         </div>
         <!-- Row Two -->
         <div class="card-group width-limit mx-auto">
-          <External site="SpaceX" image="spacex.jpg" link="https://www.spacex.com/" />
-          <External site="Rocket Lab" image="rocketLab.jpg" link="https://www.rocketlabusa.com/" />
-          <External site="Firefly Aerospace" image="firefly.png" link="https://firefly.com/" />
+          <External
+            site="SpaceX"
+            image="spacex.jpg"
+            link="https://www.spacex.com/"
+          />
+          <External
+            site="Rocket Lab"
+            image="rocketLab.jpg"
+            link="https://www.rocketlabusa.com/"
+          />
+          <External
+            site="Firefly Aerospace"
+            image="firefly.png"
+            link="https://firefly.com/"
+          />
         </div>
         <!-- Row Three -->
         <div class="card-group width-limit mx-auto">
@@ -56,13 +84,20 @@
             image="reddit.jpeg"
             link="https://www.reddit.com/r/spaceporn/top/?t=all"
           />
-          <External site="Space.com" image="spacecom.png" link="https://www.space.com/" />
+          <External
+            site="Space.com"
+            image="spacecom.png"
+            link="https://www.space.com/"
+          />
         </div>
         <div class="text-center">
           <hr />
-          <p
-            class="text-secondary font-italic"
-          >Links provided are simply useful resources to stay up to date, not to be used as evidence of relationship between organization & Rocket Downrange.</p>
+          <p class="text-secondary font-italic">
+            Rocket Downrange is an independent hobby project by a guy learning
+            Vuejs. Links provided are to other resources I find useful or
+            interesting. No partnerships exist with any of these organizations
+            and Rocket Downrange.
+          </p>
         </div>
       </div>
     </div>
