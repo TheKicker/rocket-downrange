dbcfcdcf11063676bdf724c2f00bd6cb29274998
The Kicker <cav.lemasters@geneva.edu>
2022-03-11 09:22:37 -0500

    Removing API Keys (finally)

--- src/components/NASA-APOD.vue
+++ src/components/NASA-APOD.vue
@@ -1,42 +1,37 @@
 <template>
-  <div id="APOD" class="APOD my-4 mx-lg-4 mx-md-0">
-    <div class="card">
-      <div class="card-body p-3 py-md-6 my-md-3">
+  <div id="APOD" class="APOD container my-2">
+    <div>
+      <div class="py-md-6 my-md-3">
         <h5 class="text-left text-secondary">
           NASA Astronomy Picture of the Day:
         </h5>
-        <hr />
         <div v-if="results.media_type == 'video'">
-          <h3 class="text-center my-4">{{ results.title }} (Video)</h3>
+          <h3 class="text-center my-3">{{ results.title }} (Video)</h3>
         </div>
         <div v-else>
-          <h3 class="text-center my-4">{{ results.title }}</h3>
+          <h3 class="text-center my-3">{{ results.title }}</h3>
         </div>
-        <div class="row">
-            <div v-if="results.media_type == 'video'" class="embed-responsive embed-responsive-16by9 mx-auto">              
+        <div class="d-flex flex-lg-row flex-column">
+          <div class="row half">
+            <div v-if="results.media_type == 'video'" class="embed-responsive embed-responsive-16by9">              
                 <iframe class="embed-responsive-item" :src="results.url" frameborder="0" allowfullscreen></iframe>
             </div>
-            <div v-else class="mx-auto">
-              <a :href="results.hdurl" target="_blank" rel="noopener">
-                <img
-                  :src="results.url"
-                  class="img-fluid"
-                  style="max-height: 70vh;"
-                  :alt="(results.title) +  ' - The NASA Astronomy Picture of the Day'"
-                />
-              </a>
+            <div v-else>
+                <a :href="results.hdurl" target="_blank" rel="noopener">
+                  <img :src="results.url" class="img-fluid" :alt="(results.title) +  ' - The NASA Astronomy Picture of the Day'"/>
+                </a>
             </div>
-        </div>
-        <div class="row container mx-auto">
-          <div class="my-4">
-            <div class="mx-auto my-2">
-              <strong>
+          </div>
+          <div class="row half">
+              <p class="text-primary">{{ results.explanation }}</p>
+              <br>
+              <div class="d-block text-center w-100">
                 <div v-if="results.media_type == 'video'" class="text-danger">
                   <a
                     :href="results.url"
                     target="_blank"
                     rel="noopener"
-                    class="btn btn-block btn-outline-primary text-secondary italic"
+                    class="btn btn-block btn-outline-primary"
                     >Watch on Youtube</a
                   >
                 </div>
@@ -45,24 +40,14 @@
                     :href="results.hdurl"
                     target="_blank"
                     rel="noopener"
-                    class="btn btn-block btn-outline-primary text-secondary italic"
-                    >HD Link from NASA.gov</a
-                  >
+                    class="btn btn-block btn-outline-primary"
+                    >HD Link from NASA.gov</a>
                 </div>
-              </strong>
-            </div>
-            <p class="my-2 text-primary">{{ results.explanation }}</p>
+              </div>
           </div>
         </div>
-        <div class="row container mx-auto d-block text-center">
-          <Social />
-        </div>
         <hr />
-        <p class="text-center">
-          <i>
-            Last Updated at {{ new Date(results.date).toLocaleString() }}, come back tomorrow for a new picture of the day!
-          </i>
-        </p>
+        <p class="text-center font-italic">Last updated at {{ new Date(results.date).toLocaleString() }}, come back tomorrow for a new picture of the day!</p>
       </div>
     </div>
   </div>
@@ -72,7 +57,7 @@
 var year = new Date().getFullYear();
 var month = new Date().getMonth() + 1;
 var day = new Date().getDate();
-var api_key = "PAdDVGUIS5Kjq97mf6JzKNKT7TohmSuSaLHAVImo"
+var api_key = process.env.VUE_APP_APOD_KEY
 var url =`https://api.nasa.gov/planetary/apod?date=${year}-${month}-${day}&api_key=`+api_key;
 
 window.axios = require("axios");
@@ -83,7 +68,7 @@ export default {
   name: "NASAapod",
   data() {
     return {
-      results: []
+      results: [],
     };
   },
   components: {
@@ -99,7 +84,19 @@ export default {
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.half{
+  width: 50%;
+  padding: 1rem 1.5rem;
+}
+
+/* On screens that are 600px or less, set the background color to olive */
+@media screen and (max-width: 600px) {
+  .half{
+    width: 100%;
+    padding: 0.5rem 0;
+  }
+}
+
 </style>
