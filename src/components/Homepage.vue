dfe96c660477c5e4fb593bacaea146c0c2fee04c
The Kicker <cav.lemasters@geneva.edu>
2022-03-17 11:11:01 -0400

    Resize home image

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -32,7 +32,7 @@ export default {
   height: 15rem;
 }
 .jumbotron {
-  background: #282c2b url("../assets/rocketdownrange.png") no-repeat center center/cover;
+  background: #282c2b url("../assets/rocketdownrange-1200x500.png") no-repeat center center/cover;
   background-attachment: fixed;
   max-width: 100%;
   height: auto;
