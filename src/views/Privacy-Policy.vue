75c608f0fd4cb97027f43ce59cf8dd54540b4ca9
The Kicker <cav.lemasters@geneva.edu>
2022-03-18 12:17:26 -0400

    Added BMAC to Privacy page :tada:

--- src/views/Privacy-Policy.vue
+++ src/views/Privacy-Policy.vue
@@ -3,33 +3,48 @@
     <div class="container-fluid text-center my-5">
       <h1>Rocket Downrange Privacy Policy</h1>
       <div class="body my-4 text-left">
-        <p>At Rocket Downrange, your privacy is our top priority.  As marketing, technology & space nerds, we are strongly aware of big tech and similar organizations collection efforts when it comes to your personal data when you use their site. There are benefits to this information like a better experience or more customized interface, but there can be dangers too - and we must be mindful of just how much data is offered and given away. We believe it is a fundamental human right that you know by who and how your data is processed. Below, you will find a general overview of what data Rocket Downrange has access to and how we use it. </p>    
+        <p>At Rocket Downrange, your privacy is our top priority.  As a marketing, technology & space nerd, I am strongly aware of big tech and similar organizations collection efforts when it comes to your personal data when you use their site. There are benefits to this information like a better experience or more customized interface, but there can be dangers too - and we must be mindful of just how much data is offered and given away. I believe it is a fundamental human right that you know by who and how your data is processed. Below, you will find a general overview of what data Rocket Downrange has access to and how we use it. </p>    
         <hr class="my-4">
         <div class="row mx-2">
             <h4>The Greeting in the Nav Bar </h4>
-            <p>The personalized greeting is a user experience idea that we like to build into major projects.  The first half is simple Javascript code behind the scenes that will poll your browser for the local time and then dynamically set the greeting to good morning / good afternoon / good night depending on your local time.  The second half will say "Guest" by default, or can be customized using the input box and submit button.  This data is not stored on any servers and Rocket Downrange cannot read or access this data in any form.  It is stored in your browser (Google Chrome, Apple Safari, Mozilla Firefox, Microsoft Edge) on your device through what's called <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage" class="link" target="_blank" rel="noopener">local storage.</a>  This is completely unique to your device and cannot be accessed or changed by anyone but you.   </p>
+            <p>The personalized greeting is a user experience idea that I like to build into major projects.  The first half is simple Javascript code behind the scenes that will poll your browser for the local time and then dynamically set the greeting to good morning / good afternoon / good night depending on your local time.  The second half will say "Guest" by default, or can be customized using the input box and submit button.  This data is not stored on any servers and Rocket Downrange cannot read or access this data.  It is stored in your browser (Google Chrome, Apple Safari, Mozilla Firefox, Microsoft Edge) on your device through what's called <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage" class="link" target="_blank" rel="noopener">local storage.</a>  This is completely unique to your device and cannot be accessed or changed by anyone but you.   </p>
         </div>
         <br>
         <div class="row mx-2">
             <h4>The Form on the Index (Home) Page</h4>
-            <p>This form is designed to provide better user experience for you, and better learning experience for us.  Aside from a few college courses, Rocket Downrange is made by a self-taught computer programmer who is still learning - this site is a way to satisfy both passion for space technology & exploration and to learn <a href="https://vuejs.org/" class="link" target="_blank" rel="noopener">Vue.js which is a Javascript framework</a> used to build modern sites.  The only way we know if we are doing a good job (or not) is through your feedback.  Everything is welcome; positive, negative or even suggestions on new features or APIs to include. All submissions are filtered through different layers of security / bot detection to verify we're getting legitimate responses from real people. If you use the form, you recognize that we reserve the right to use the information in any capacity towards the mission of the site.  WE WILL NOT share your name, email address or message with anyone except when required by law.   </p>
+            <p>This form is designed to provide better user experience for you, and better learning experience for me.  Aside from a few college courses, Rocket Downrange is made by a self-taught computer programmer who is still learning - this site is a way to satisfy both passion for space technology & exploration and to learn <a href="https://vuejs.org/" class="link" target="_blank" rel="noopener">Vue.js which is a Javascript framework</a> used to build modern sites.  The only way I can know if I am doing a good job (or not) is through your feedback.  Everything is welcome; positive, negative or even suggestions on new features or APIs to include. All submissions are filtered through different layers of security / bot detection to verify I am getting legitimate responses from real people. If you use the form, you recognize that I reserve the right to use the information in any capacity towards the mission of the site.  I WILL NOT share your name, email address or message with anyone except when required by law.   </p>
         </div>
         <br>
         <div class="row mx-2">
             <h4>Google Analytics & Google AdSense </h4>
             <p>This site uses Google Analytics to better understand how our users engage with the site.  This aggregated information does not include individual names or personal data, so it does not personally identify you or any other user. All data is anonymized by default and only reveals general specifications like device type (computer, mobile, tablet), location data (nearest city, state, country), language (English, Chinese, Russian), etc. This allows Rocket Downrange to better understand who is using our site, how they are using it and what issues they may be having.  It also allows us to see how our site progresses over time, what features people love and which ones don't perform as expected - we want to see the site grow :)</p>
-            <p>Operating a website is not free! However, the goal is not to cheapen the experience through hundreds of ads. Google AdSense has been added to the site in limited amounts to offset this, or if you would like to directly support the site, <a href="https://cavlemasters.com/contact" target="_blank" rel="noopener" class="link">you can send a message</a> for a PayPal link. All tips are greatly appreciated however they are not necessary and non-refundable.</p>
+            <!-- <p>Operating a website is not free! However, the goal is not to cheapen the experience through hundreds of ads. Google AdSense has been added to the site in limited amounts to offset this, or if you would like to directly support the site, <a href="https://cavlemasters.com/contact" target="_blank" rel="noopener" class="link">you can send a message</a> for a PayPal link. All tips are greatly appreciated however they are not necessary and non-refundable.</p> -->
             <p>If you would like to opt out of Google Analytics tracking, you may do so at <a href="https://tools.google.com/dlpage/gaoptout/" target="_blank" rel="noopener" class="link">this link.</a>  Members of the EU and other nations note that by using Rocket Downrange, you accept that your data is read/used/updated by these companies to enable Rocket Downrange to provide better content and site structure. For example, how Google keeps your data anonymized and uses it can be found <a href="https://policies.google.com/technologies/partner-sites" target="_blank" rel="noopener" class="link">here.</a> </p>
+        
+            <hr>
+            <p>Update: For the forseeable future, Google Adsense has been disabled.  I felt like it sort of cheapened the experience of the site, so I have ditched it in favor of Buy Me a Coffee instead.  This way you can directly support the project (if you'd like) without selling your soul to the devil (Google, I'm kidding, kind of.)</p>
+        </div>
+        <br>
+        <div class="row mx-2 mb-1">
+            <h4>Buy Me a Coffee</h4>
+            <p>As the project begins to mature, I have had some people support the project in a variety of ways.  While not necessary to use and enjoy the site, I appreciate the investment into my skills and the continuation of working on such an awesome project.  For a platform, I've chosen <a href="https://www.buymeacoffee.com/cavlemasters" target="_blank" rel="noopener nofollow" class="link">Buy Me a Coffee</a> because of it's easy to use and clean interface - plus the ability to write little mini blogs about things I've changed or added to the site as time goes on.  </p>
+            <p>Donations are not necessary, but are greatly appreciated. If you choose to donate, you accept that you are not getting any direct products or services in return and cannot be refunded. </p>
+            <div id="desktop" class="mx-auto my-3">
+              <a href="https://www.buymeacoffee.com/cavlemasters" target="_BLANK" rel="noopener"><img class="img-fluid" src="@/assets/bmcbrand/bmc-button.png" border="0"></a>
+            </div>
+            <div id="mobile" class="mx-auto my-2">
+              <a href="https://www.buymeacoffee.com/cavlemasters" target="_BLANK" rel="noopener"><img style="max-width: 100%;" src="@/assets/bmcbrand/bmc-button.png" border="0"></a>
+            </div>
         </div>
         <br>
         <div class="row mx-2 mb-1">
             <h4>Edabit</h4>
             <p>The basis of this website is to continue learning and refining my skills as a junior developer. <a href="https://edabit.com/?ref=cavanlemasters" target="_blank" rel="noopener nofollow" class="link">Edabit</a> is a site comprised of byte-sized (haha lame computer joke) challenges in a variety of languages that you can use to practice the fundamentals.  At the end of 2020, I was invited to be an affiliate, this means that any links you see are sponsored. I would not support them if I did not fully believe in how beneficial they are to learning the basics of programming. </p>
             <div id="desktop" class="mx-auto my-3">
-              <a href="https://edabit.com?ref=cavanlemasters" target="_BLANK" rel="noopener"><img src="@/assets/edabit-wide.png" border="0"></a>
+              <a href="https://edabit.com?ref=cavanlemasters" target="_BLANK" rel="noopener"><img src="@/assets/webp/edabit-wide.webp" border="0"></a>
             </div>
             <div id="mobile" class="mx-auto my-2">
-              <a href="https://edabit.com?ref=cavanlemasters" target="_BLANK" rel="noopener"><img src="@/assets/edabit-short.png" border="0"></a>
+              <a href="https://edabit.com?ref=cavanlemasters" target="_BLANK" rel="noopener"><img src="@/assets/webp/edabit-short.webp" border="0"></a>
             </div>
             <p class="mx-auto">Start your development journey today, and tomorrow you can directly contribute to Rocket Downrange!</p>
         </div>
@@ -41,7 +56,7 @@
         <br>
         <div class="row mx-2">
             <h4>Further Questions</h4>
-            <p>If you have any other questions, would like to help the site grow or simply want to talk about the final frontier - you can use the contact form on the home page, follow us on Twitter <a href="https://twitter.com/rocketdownrange" class="link" target="_blank" rel="noopener">(<i class="fab fa-twitter"></i> @RocketDownrange )</a>.  We look forward to exploring with you! </p>
+            <p>If you have any other questions, would like to help the site grow or simply want to talk about the final frontier - you can use the contact form on the home page, or follow on Twitter <a href="https://twitter.com/rocketdownrange" class="link" target="_blank" rel="noopener">(<i class="fab fa-twitter"></i> @RocketDownrange )</a>.  We look forward to exploring with you! </p>
         </div>
         <br>
         <div class="spacer"></div>
